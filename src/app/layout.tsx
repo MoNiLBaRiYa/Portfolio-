3115df246bca84d17d67d0e29358f9034b7d0967
MasterM76 <2203051050357@paruluniversity.ac.in>
2026-01-21 17:50:51 +0530

    Update favicon with new logo for browser tab and PWA icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,9 +78,10 @@ export default function RootLayout({
 
         {/* Favicon and app icons */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* Performance hints */}
         <meta
